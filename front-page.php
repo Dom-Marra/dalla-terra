cba6d1959c16de5d61a600585029153b1da493b0
Dom-Marra <dominicmarra321@gmail.com>
2021-10-29 22:56:48 -0700

    Added categories to front-page

--- front-page.php
+++ front-page.php
@@ -17,110 +17,102 @@ get_header();
 
 	<main id="primary" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			?>
-			<section class="view-single-product-home">
+		<section class="view-single-product-home">
+			<?php 
+			$featured_posts = get_field('view_single_product');
+			if( $featured_posts ): ?>
+			
+				<?php foreach( $featured_posts as $post ): 
+			
+					// Setup this post for WP functions (variable must be named $post).
+					setup_postdata($post); ?>
+					<?php the_post_thumbnail() ?>
+						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+				<?php endforeach; ?>
+				
 				<?php 
-				$featured_posts = get_field('view_single_product');
-				if( $featured_posts ): ?>
-				
-					<?php foreach( $featured_posts as $post ): 
-				
-						// Setup this post for WP functions (variable must be named $post).
-						setup_postdata($post); ?>
-						<?php the_post_thumbnail() ?>
-							<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-					<?php endforeach; ?>
-					
-					<?php 
-					// Reset the global post object so that the rest of the page works correctly.
-					wp_reset_postdata(); ?>
-				<?php endif; ?>
-			</section>
+				// Reset the global post object so that the rest of the page works correctly.
+				wp_reset_postdata(); ?>
+			<?php endif; ?>
+		</section>
 
 
-			</section class="custom-cta-home"> 
-				<?php
-				$customcta = get_field('custom_call_to_action');
-				if( $customcta ): ?>
-					<div id="custom-cta-content">
-
-					<img src="<?php echo esc_url( $customcta['background_image']['url'] ); ?>" alt="<?php echo esc_attr( $customcta['background_image']['alt'] ); ?>" />
-					<a href="<?php echo esc_url( $customcta['link']['url'] ); ?>">
-						<button><?php echo esc_html( $customcta['button_text']); ?></button>
-					</a>
-						
-					</div>
-					</style>
-				<?php endif; ?>
-			<section>
-
-			<section class="view-shop-cta-home">
+		</section class="custom-cta-home"> 
 			<?php
-				$viewShopCTA= get_field('view_shop_cta');
+			$custom_cta = get_field('custom_call_to_action');
+			if( $custom_cta ): ?>
+				<div id="custom-cta-content">
+
+				<img src="<?php echo esc_url( $custom_cta['background_image']['url'] ); ?>" alt="<?php echo esc_attr( $custom_cta['background_image']['alt'] ); ?>" />
+				<a href="<?php echo esc_url( $custom_cta['link']['url'] ); ?>">
+					<?php echo esc_html( $custom_cta['button_text']); ?>
+				</a>
+					
+				</div>
+			<?php endif; ?>
+		<section>
+
+		<section class="view-shop-cta-home">
+			<?php
+				$view_shop_CTA= get_field('view_shop_cta');
 				if( $viewShopCTA ): ?>
 					<div id="custom-cta-content">
 
-					<img src="<?php echo esc_url( $viewShopCTA['background_image']['url'] ); ?>" alt="<?php echo esc_attr( $viewShopCTA['background_image']['alt'] ); ?>" />
-					<a href="<?php echo esc_url( $viewShopCTA['link']['url'] ); ?>">
-						<button><?php echo esc_html( $viewShopCTA['button_text']); ?></button>
+					<img src="<?php echo esc_url( $view_shop_CTA['background_image']['url'] ); ?>" alt="<?php echo esc_attr( $view_shop_CTA['background_image']['alt'] ); ?>" />
+					<a href="<?php echo esc_url( $view_shop_CTA['link']['url'] ); ?>">
+						<?php echo esc_html( $view_shop_CTA['button_text']); ?>
 					</a>
 						
 					</div>
-					</style>
-				<?php endif; ?>
-			</section>
+			<?php endif; ?>
+		</section>
 
-			<section class="bestsellers-home">
-				<?php 
-				$featured_posts = get_field('bestsellers');
-				if( $featured_posts ): ?>
-				
-					<?php foreach( $featured_posts as $post ): 
-				
-						// Setup this post for WP functions (variable must be named $post).
-						setup_postdata($post); ?>
-						<?php the_post_thumbnail() ?>
-							<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-					<?php endforeach; ?>
-					
-					<?php 
-					// Reset the global post object so that the rest of the page works correctly.
-					wp_reset_postdata(); ?>
-				<?php endif; ?>
-			</section>
+		<section class="bestsellers-home">
+			<?php 
+			$featured_posts = get_field('bestsellers');
+			if( $featured_posts ): ?>
 			
-			<section class="categories-section-home">
-			 <?php
-				//Inserty code for Catergory Taxonomy
-			 ?>
-			</section>
-
-
-			<section class="about-us-banner-home">
+				<?php foreach( $featured_posts as $post ): 
+			
+					// Setup this post for WP functions (variable must be named $post).
+					setup_postdata($post); ?>
+					<?php the_post_thumbnail() ?>
+						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+				<?php endforeach; ?>
+				
+				<?php 
+				// Reset the global post object so that the rest of the page works correctly.
+				wp_reset_postdata(); ?>
+			<?php endif; ?>
+		</section>
+	
+		<section class="categories-section-home">
 			<?php
-				$aboutUsBanner= get_field('about_us_banner');
+				$terms = get_field('categories');
+
+				if (!empty($terms)):
+					set_query_var( 'categories', $terms ); 
+					get_template_part( 'template-parts/content', 'dalla-terra-categories' );
+				endif;
+			?>
+		</section>
+
+
+		<section class="about-us-banner-home">
+			<?php
+				$about_us_banner= get_field('about_us_banner');
 				if( $aboutUsBanner ): ?>
 					<div id="about-us-banner">
 
-					<img src="<?php echo esc_url( $aboutUsBanner['background_image']['url'] ); ?>" alt="<?php echo esc_attr( $aboutUsBanner['background_image']['alt'] ); ?>" />
-					<a href="<?php echo esc_url( $aboutUsBanner['link']['url'] ); ?>">
-						<button><?php echo esc_html( $aboutUsBanner['button_text']); ?></button>
+					<img src="<?php echo esc_url( $about_us_banner['background_image']['url'] ); ?>" alt="<?php echo esc_attr( $about_us_banner['background_image']['alt'] ); ?>" />
+					<a href="<?php echo esc_url( $about_us_banner['link']['url'] ); ?>">
+						<?php echo esc_html( $about_us_banner['button_text']); ?>
 					</a>
 						
 					</div>
 					</style>
 				<?php endif; ?>
-			</section>
-
-
-			
-		<?php endwhile; // End of the loop.
-		?>
-
+		</section>
 	</main><!-- #main -->
 
 <?php
