55289f67847345edce0adbd7e80b15329233417f
sarahbains <87668267+sarahbains@users.noreply.github.com>
2021-11-09 22:20:01 -0800

    fix aout page margin

--- page-about-us.php
+++ page-about-us.php
@@ -16,119 +16,120 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-
-		<?php
-		while ( have_posts() ) :
-			the_post();
-        ?>
-        <section class="about-us-banner">
+        <div class="about-us-page-content">
             <?php
-            $aboutUsBanner = get_field('banner');
-				if( $aboutUsBanner ): ?>
-					<div id="custom-cta-content">
-                        <?php echo wp_get_attachment_image($aboutUsBanner['background_image'], 'full' ); ?>
-                        <h2><?php echo $aboutUsBanner['text']; ?></h2>
-					</div>
-				<?php endif; ?>
-        </section>
-
-        <section class="about-us-quote">
-                    <blockquote><?php the_field('quote'); ?></blockquote>
-        </section>
-       
-        <section class="about-us-farm-gallery">
-        <?php
-            $farmGallery = get_field('farm_gallery');
-            $images = $farmGallery['images'];
-
-				if( $farmGallery): ?>
-					<h2><?php echo $farmGallery['gallery_heading']; ?></h2>
-                    <p><?php echo $farmGallery['gallery_caption']; ?></p>
+            while ( have_posts() ) :
+                the_post();
+            ?>
+            <section class="about-us-banner">
+                <?php
+                $aboutUsBanner = get_field('banner');
+                    if( $aboutUsBanner ): ?>
+                        <div id="custom-cta-content">
+                            <?php echo wp_get_attachment_image($aboutUsBanner['background_image'], 'full' ); ?>
+                            <h2><?php echo $aboutUsBanner['text']; ?></h2>
+                        </div>
                     <?php endif; ?>
-
-                    <div class="farm-gallery">
-                        <?php
-                        if( $images ): ?>
-                            <ul>
-                                <?php foreach( $images as $image ): ?>
-                                    <li>
-                                        <a href="<?php echo $image['url']; ?>">
-                                        <?php echo wp_get_attachment_image($image, 'medium' ); ?>
-                                        </a>
-                                        <p><?php echo $image['caption']; ?></p>
-                                    </li>
-                                <?php endforeach; ?>
-                            </ul>
-                        <?php endif; ?>
-        </section>
-
-        <div class="about-us-mission-history-vision">
-            <section class="about-us-mission">
-            <?php $field = get_field_object('our_mission');?>
-                <h2><?php echo $field['label']; ?></h2>
-                <p><?php the_field('our_mission'); ?></p>
             </section>
 
-            <section class="about-us-history">
-            <?php
-                $field = get_field_object('our_history');
-                ?>
-                <h2><?php echo $field['label']; ?></h2>
-                <p><?php the_field('our_history'); ?></p>
+            <section class="about-us-quote">
+                        <blockquote><?php the_field('quote'); ?></blockquote>
             </section>
-        </div>
-
-        <section class="about-us-winery-gallery">
-        <?php
-            $wineryGallery = get_field('winery_gallery');
-            $images = $wineryGallery['images'];
-
-				if( $wineryGallery ): ?>
-					<h2><?php echo $wineryGallery['gallery_heading']; ?></h2>
-                    <p><?php echo $wineryGallery['gallery_caption']; ?></p>
-                    <?php endif; ?>
-
-                    <div class="winery-gallery">
-                        <?php
-                        if( $images ): ?>
-                            <ul>
-                                <?php foreach( $images as $image ): ?>
-                                    <li>
-                                        <a href="<?php echo $image['url']; ?>">
-                                            <?php echo wp_get_attachment_image($image, 'medium' ); ?>
-                                        </a>
-                                        <p><?php echo $image['caption']; ?></p>
-                                    </li>
-                                <?php endforeach; ?>
-                            </ul>
-                        <?php endif; ?>
-        </section>
         
-        <div class="about-us-mission-history-vision">
-            <section class="about-us-vision">
-            <?php $field = get_field_object('our_vision');?>
-                <h2><?php echo $field['label']; ?></h2>
-                <p><?php the_field('our_vision'); ?></p>
+            <section class="about-us-farm-gallery">
+            <?php
+                $farmGallery = get_field('farm_gallery');
+                $images = $farmGallery['images'];
+
+                    if( $farmGallery): ?>
+                        <h2><?php echo $farmGallery['gallery_heading']; ?></h2>
+                        <p><?php echo $farmGallery['gallery_caption']; ?></p>
+                        <?php endif; ?>
+
+                        <div class="farm-gallery">
+                            <?php
+                            if( $images ): ?>
+                                <ul>
+                                    <?php foreach( $images as $image ): ?>
+                                        <li>
+                                            <a href="<?php echo $image['url']; ?>">
+                                            <?php echo wp_get_attachment_image($image, 'medium' ); ?>
+                                            </a>
+                                            <p><?php echo $image['caption']; ?></p>
+                                        </li>
+                                    <?php endforeach; ?>
+                                </ul>
+                            <?php endif; ?>
             </section>
 
-            <section class="link-cta">
+            <div class="about-us-mission-history-vision">
+                <section class="about-us-mission">
+                <?php $field = get_field_object('our_mission');?>
+                    <h2><?php echo $field['label']; ?></h2>
+                    <p><?php the_field('our_mission'); ?></p>
+                </section>
+
+                <section class="about-us-history">
+                <?php
+                    $field = get_field_object('our_history');
+                    ?>
+                    <h2><?php echo $field['label']; ?></h2>
+                    <p><?php the_field('our_history'); ?></p>
+                </section>
+            </div>
+
+            <section class="about-us-winery-gallery">
             <?php
-                    $shopLink= get_field('link');
-                    if( $shopLink ): ?>
-                        <a class="view-our-shop" href="<?php echo esc_url( $shopLink['link']['url'] ); ?>">
-                            <?php echo esc_html( $shopLink['text']); ?>
-                        </a>
-                            
-                    <?php endif; ?>
-                    
-            <nav id="social-navigation" class="social-navigation">
-                    <?php wp_nav_menu( array( 'theme_location' => 'social')); ?>
-            </nav>
+                $wineryGallery = get_field('winery_gallery');
+                $images = $wineryGallery['images'];
+
+                    if( $wineryGallery ): ?>
+                        <h2><?php echo $wineryGallery['gallery_heading']; ?></h2>
+                        <p><?php echo $wineryGallery['gallery_caption']; ?></p>
+                        <?php endif; ?>
+
+                        <div class="winery-gallery">
+                            <?php
+                            if( $images ): ?>
+                                <ul>
+                                    <?php foreach( $images as $image ): ?>
+                                        <li>
+                                            <a href="<?php echo $image['url']; ?>">
+                                                <?php echo wp_get_attachment_image($image, 'medium' ); ?>
+                                            </a>
+                                            <p><?php echo $image['caption']; ?></p>
+                                        </li>
+                                    <?php endforeach; ?>
+                                </ul>
+                            <?php endif; ?>
             </section>
+            
+            <div class="about-us-mission-history-vision">
+                <section class="about-us-vision">
+                <?php $field = get_field_object('our_vision');?>
+                    <h2><?php echo $field['label']; ?></h2>
+                    <p><?php the_field('our_vision'); ?></p>
+                </section>
+
+                <section class="link-cta">
+                <?php
+                        $shopLink= get_field('link');
+                        if( $shopLink ): ?>
+                            <a class="view-our-shop" href="<?php echo esc_url( $shopLink['link']['url'] ); ?>">
+                                <?php echo esc_html( $shopLink['text']); ?>
+                            </a>
+                                
+                        <?php endif; ?>
+                        
+                <nav id="social-navigation" class="social-navigation">
+                        <?php wp_nav_menu( array( 'theme_location' => 'social')); ?>
+                </nav>
+                </section>
+            </div>
+            
+            <?php endwhile; // End of the loop.
+            ?>
         </div>
-		
-        <?php endwhile; // End of the loop.
-		?>
 
 	</main><!-- #main -->
 
